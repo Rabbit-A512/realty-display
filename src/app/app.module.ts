59cb6313eb7c2ae7ceece2c3fde1278745403b24
Rabbit-A512 <2654525303@qq.com>
2018-06-13 11:40:35 +0800

    test carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MyCarouselComponent } from './my-carousel/my-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
